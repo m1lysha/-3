3d8785b7f5ea093c9ab27641ccd7d977f920b94d
RobotComp.ru <RobotComp.ru@DESKTOP-94TEC7C>
2024-10-10 16:59:05 +0300

    Добавьте файлы проекта.

---
+++ лаб3/Program.cs
@@ -0,0 +1,11 @@
+﻿try
+{
+    Console.Write("Введите номер места товара: ");
+    int x = int.Parse(Console.ReadLine());
+    Console.WriteLine($"Секция={(x / 120) + 1}");
+    Console.WriteLine($"Ярус={((x % 120) / 15) + 1}");
+}
+catch
+{
+    Console.WriteLine("Введите правильно!");
+}
